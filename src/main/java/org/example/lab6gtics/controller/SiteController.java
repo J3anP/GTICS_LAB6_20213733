a379007638f91c35581a9d3c0603b1b72cf90f59
J3anP <a20213733@pucp.edu.pe>
2024-05-09 20:43:08 -0500

    ola

--- src/main/java/org/example/lab6gtics/controller/SiteController.java
+++ src/main/java/org/example/lab6gtics/controller/SiteController.java
@@ -2,6 +2,8 @@ package org.example.lab6gtics.controller;
 
 import org.example.lab6gtics.repository.SiteRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +14,11 @@ public class SiteController {
 
     public SiteController(SiteRepository siteRepository) {
         this.siteRepository = siteRepository;
+    }
+
+    @GetMapping(value = {"", "/", "estadistica"})
+    public String estadisticaSitio(Model model) {
+
+        return "sitios/estadistica";
     }
 }
